c3639338be7d0dea18d1fb4cd164e27fa62c23b2
Augusto Gunsch <augustogunsch@tutanota.com>
2021-08-10 21:47:07 -0300

    Improve install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-OLDDIR=$(pwd)
 MYDIR=$(dirname "$0")
-cd "$MYDIR"
-MYDIR=$(pwd)
-cd "$OLDDIR"
-ln -sf "$MYDIR/dwm_bar.sh" /usr/bin/dwm_bar
+pushd "$MYDIR"
+ln -sf "./dwm_bar.sh" "$HOME/.local/bin/dwm_bar"
+popd
